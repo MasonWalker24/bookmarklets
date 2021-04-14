4739a8f64dd10cddcf163de20525a0ac92fe93d0
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-14 10:55:49 -0400

    Update rickroll.js

--- rickroll.js
+++ rickroll.js
@@ -1,1 +1,19 @@
-
+javascript:
+var link = document.getElementsByTagName("a")[0];
+var att = document.createAttribute("href");
+att.value = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+link.setAttributeNode(att);
+var link2 = document.getElementsByTagName("a")[1];
+link2.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var link3 = document.getElementsByTagName("a")[2];
+link3.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var link4 = document.getElementsByTagName("a")[3];
+link4.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var link5 = document.getElementsByTagName("a")[4];
+link5.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var link6 = document.getElementsByTagName("a")[5];
+link6.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var link7 = document.getElementsByTagName("a")[6];
+link7.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var length = link.length;
+alert(length);

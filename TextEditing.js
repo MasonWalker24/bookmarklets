a465a49d2bb5e5fc6109a0cd0d46ed49a1eaa574
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-14 08:51:30 -0400

    Update TextEditing.js

--- TextEditing.js
+++ TextEditing.js
@@ -1,5 +1,19 @@
 javascript:
 var att = document.createAttribute("contenteditable"); 
+var button = document.createElement("BUTTON");
+button.style.position = "fixed";
+button.style.width = "10%";
+button.style.height = "15%";
+button.style.background = "none";
+button.style.border = "2px solid black";
+button.style.top = "85%";
+button.style.left = "90%";
+button.style.zIndex = "100";
+button.innerHTML = "Turn off";
+button.addEventListener('click', function() {
+    att.value = "false";
+}, false);
+
 if(att.value == "true")
 {
 att.value = "false";
@@ -8,4 +22,6 @@ else
 {
   att.value = "true";
 }
+
 document.body.setAttributeNode(att);
+document.body.appendChild(button);

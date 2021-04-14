71883d866d940b9e45bf573fde7b26bdf4b153cf
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-14 08:55:56 -0400

    Update TextEditing.js

--- TextEditing.js
+++ TextEditing.js
@@ -4,14 +4,21 @@ var button = document.createElement("BUTTON");
 button.style.position = "fixed";
 button.style.width = "10%";
 button.style.height = "15%";
-button.style.background = "none";
+button.style.backgroundColor = "white";
 button.style.border = "2px solid black";
 button.style.top = "85%";
 button.style.left = "90%";
 button.style.zIndex = "100";
-button.innerHTML = "Turn off";
+button.style.fontSize = "1.8vw";
+button.innerHTML = "Toggle";
 button.addEventListener('click', function() {
+    if(att.value == "true")
+    {
     att.value = "false";
+    } else 
+    {
+     att.value = "true";   
+    }
 }, false);
 
 if(att.value == "true")

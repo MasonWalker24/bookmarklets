6bf40b47f3124e47f1bcfde70ea0bfaf0caee563
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 10:31:20 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,2 +1,6 @@
 var h1 = document.getElementsByTagName("h1")[0];
-h1.style.textShadow = "0px 0px 30px blue";
+h1.setAttribute("onmouseover", "add()");
+function add()
+{
+ h1.style.textShadow = "0px 0px 30px blue"; 
+}

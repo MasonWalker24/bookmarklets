0c3309ff52467a9680ab75b5ef4ad97c36abc6a5
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 10:36:14 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,6 +1,11 @@
-var h1 = document.getElementsByTagName("h1")[0];
-h1.setAttribute("onmouseover", "add()");
+var h1 = document.getElementsByTagName("h1");
+h1length = h1.length;
+for(var i = 0; i <= h1length; i++)
+{
+ var h1_2 = h1[i];
+h1_2.setAttribute("onmouseover", "add()");
 function add()
 {
- h1.style.textShadow = "0px 0px 30px blue"; 
+ h1_2.style.textShadow = "0px 0px 30px blue"; 
+}
 }

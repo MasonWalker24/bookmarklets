81870f32f0822abb9d68abd35d00931fb9e1183a
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 11:13:55 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,5 +1,5 @@
 var h1 = document.getElementsByTagName("h1");
 h1length = h1.length - 1;
 for (var i=0;i<h1.length+1; i++) {
-             h1[i].style.textShadow = "2px 2px 0px blue";           
+             h1[i].style.textShadow = "2px 2px 5px blue";           
 }

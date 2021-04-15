cfb96994d81daca64534156270ce6af85c887ec2
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 11:51:33 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -2,9 +2,9 @@ var h1 = document.getElementsByTagName("h1");
 h1length = h1.length - 1;
 for (var i=0;i<h1.length+1; i++) 
 {
-    h1[i].addEventListener("mouseover", lightUp);
-    function lightUp()
+    h1[i].addEventListener("mouseover", lightUp());
+}
+function lightUp()
 {
  h1[i].style.textShadow = "2px 2px 5px blue"; 
-}
 }

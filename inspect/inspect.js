2afab5d52e354f87c41fc07882be43cdf2197f63
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 08:45:39 -0400

    Update inspect.js

--- inspect/inspect.js
+++ inspect/inspect.js
@@ -1,4 +1,4 @@
 var head = document.getElementsByName("head")[0];
 var scriptlink = document.createElement("LINK");
-scriptlink.href = "https://cubesniper24.github.io/bookmarklets/inspect/inspectscript.js";
+scriptlink.href = "https://cubesniper24.github.io/bookmarklets/inspect/inspectScript.js";
 head.appendChild(scriptlink);

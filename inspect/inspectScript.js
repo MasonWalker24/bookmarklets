dfc2327713f899f739058f81216568efa411838a
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 10:51:21 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,5 +1,9 @@
 var h1 = document.getElementsByTagName("h1");
 h1length = h1.length - 1;
 for (var i=0;i<h1.length+1; i++) {
-            h1[i].setAttribute("onmouseover", 'function() {h1[i].style.boxShadow = "0px 0px 30px blue";}')
+            h1[i].createAttribute("onmouseover", "lightUp");
+            function lightUp()
+            {
+             h1[i].style.textShadow = "0px 0px 15px blue";           
+            }
 }

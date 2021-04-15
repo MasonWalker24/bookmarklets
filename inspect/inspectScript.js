31eaae0ef575dbf94342afccb7872b6e3b77a3cc
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 10:41:15 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,5 +1,5 @@
 var h1 = document.getElementsByTagName("h1");
 h1length = h1.length - 1;
-for (var i=0;i<h1.length; i++) {
+for (var i=0;i<h1.length+1; i++) {
             h1[i].style.boxShadow = "0px 0px 30px blue";
         }

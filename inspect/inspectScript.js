39bd8395e8b06e0201187bf0d86f09bd34c212ba
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-15 09:09:35 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -1,2 +1,13 @@
-var test = "test";
-alert(test);
+var h1 = document.getElementsByTagName("h1");
+var h1length = h1.length;
+int i = 0;
+for(i <= h1length)
+{
+ h1[i].createAttribute("onmouseover", "lightUp()");
+  function lightUp()
+{
+  h1[i].style.boxShadow = "0px 0px 30px blue"
+}
+}
+
+

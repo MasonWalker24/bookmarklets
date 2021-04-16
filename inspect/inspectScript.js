f45869100129577ee49f48cf5c30aefffeedcac9
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-04-16 10:31:29 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -111,7 +111,7 @@ function clickh1(event)
     var h1parent = event.target.parentElement;
     var h1 = event.target;
     var h1text = h1.innerHTML;
-    var h1class = h1.getElement("class");
+    //var h1class = h1.getElement("class");
     //input.innerHTML = '&60;h1 class="' + h1class + '"' + '&62;' + h1text + '&60;/h1&62';
     document.body.appendChild(input);
     document.body.appendChild(button);

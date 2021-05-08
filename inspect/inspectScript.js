cf8fa6637fd60867957803dc51a966c0854b61da
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-08 12:37:48 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -38,8 +38,8 @@ input.style.border = "2px solid black";
 input.style.zIndex = "101";
 input.style.color = "white";
 
-//console.style.position = "fixed";
-//console.style.width = "80%";
+console.style.position = "fixed";
+console.style.width = "80%";
 //consoleAtt = document.createAttribute("contenteditable");
 //consoleAtt.value = "true";
 //console.setAttributeNode(inputAtt);

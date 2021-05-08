3791ab0f59d561b21e929973bcbdc91ee5a788b8
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-08 11:44:55 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -63,22 +63,22 @@ container.style.left = "0";
 
 
 //console functions and variables
-var isConsole = false;
-function changeConsole()
-{
-  if(isConsole == false)
-  {
-      document.body.appendChild(console);
-      document.body.appendChild(container);
-      consolebutton.innerHTML = "Close Console";
-  }
-  else
-  {
-      console.remove();
-      container.remove();
-      consolebutton.innerHTML = "Open Console";
-  }
-}
+//var isConsole = false;
+//function changeConsole()
+//{
+  //if(isConsole == false)
+  //{
+    //  document.body.appendChild(console);
+      //document.body.appendChild(container);
+      //consolebutton.innerHTML = "Close Console";
+  //}
+  //else
+  //{
+    //  console.remove();
+      //container.remove();
+      //consolebutton.innerHTML = "Open Console";
+  //}
+//}
 
 //add all h1 elements
 const headlines = document.getElementsByTagName("h1");

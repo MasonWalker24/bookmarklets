97826914e2de169823a730d0503f4aad5c7b3cf3
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-10 09:22:30 -0400

    Update inspectScript.js

--- inspect/inspectScript.js
+++ inspect/inspectScript.js
@@ -176,16 +176,16 @@ function clickelement(event)
 const headlines2 = document.getElementsByTagName("h2");
 for (const headline2 of headlines2) {
     headline2.addEventListener("mouseover", lightUp, false);
-    headline2.addEventListener("mouseout", unLightUp, false);
-    headline2.addEventListener("click", clickh2, false);
+    //headline2.addEventListener("mouseout", unLightUp, false);
+    //headline2.addEventListener("click", clickh2, false);
 }
 //add all h3 elements
 
 const headlines3 = document.getElementsByTagName("h3");
 for (const headline3 of headlines3) {
     headline3.addEventListener("mouseover", lightUp, false);
-    headline3.addEventListener("mouseout", unLightUp, false);
-    headline3.addEventListener("click", clickh3, false);
+    //headline3.addEventListener("mouseout", unLightUp, false);
+    //headline3.addEventListener("click", clickh3, false);
 }
 
 //add all h4 elements
@@ -193,7 +193,7 @@ for (const headline3 of headlines3) {
 const headlines4 = document.getElementsByTagName("h4");
 for (const headline4 of headlines4) {
     headline4.addEventListener("mouseover", lightUp, false);
-    headline4.addEventListener("mouseout", unLightUp, false);
+    //headline4.addEventListener("mouseout", unLightUp, false);
    // headline4.addEventListenr("click", clickh4, false);
 }
 
@@ -203,8 +203,8 @@ for (const headline4 of headlines4) {
 const paragraphs = document.getElementsByTagName("p");
 for (const paragraph of paragraphs) {
     paragraph.addEventListener("mouseover", lightUp, false);
-    paragraph.addEventListener("mouseout", unLightUp, false);
-    paragraph.addEventListener("click", clickp, false);
+    //paragraph.addEventListener("mouseout", unLightUp, false);
+    //paragraph.addEventListener("click", clickp, false);
 }
 
 //add all A elements
@@ -223,8 +223,8 @@ for (const link of links) {
 const images = document.getElementsByTagName("img");
 for (const image of images) {
     image.addEventListener("mouseover", lightUp, false);
-    image.addEventListener("mouseout", unLightUp, false);
-    image.addEventListener("click", clickimg, false);
+    //image.addEventListener("mouseout", unLightUp, false);
+    //image.addEventListener("click", clickimg, false);
 }
 
 //add all div elements
@@ -232,7 +232,7 @@ for (const image of images) {
 const divs = document.getElementsByTagName("div");
 for (const div of divs) {
     div.addEventListener("mouseover", lightUp, false);
-    div.addEventListener("mouseout", unLightUp, false);
+    //div.addEventListener("mouseout", unLightUp, false);
     //div.addEventListener("click", clickdiv, false);
 }
 
@@ -241,8 +241,8 @@ for (const div of divs) {
 const buttons = document.getElementsByTagName("button");
 for (const button of buttons) {
     button.addEventListener("mouseover", lightUp, false);
-    button.addEventListener("mouseout", unLightUp, false);
-    button.addEventListener("click", clickbtn, false);
+    //button.addEventListener("mouseout", unLightUp, false);
+    //button.addEventListener("click", clickbtn, false);
 }
 document.body.appendChild(consolebutton);
 

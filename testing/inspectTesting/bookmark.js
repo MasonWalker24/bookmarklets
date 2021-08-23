fa851e2670e2839d4c913c31165576f0f76e3be6
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-23 12:26:08 -0400

    Update bookmark.js

--- testing/inspectTesting/bookmark.js
+++ testing/inspectTesting/bookmark.js
@@ -1,1 +1,4 @@
-
+javascript:
+var scriptlink = document.createElement("script");
+scriptlink.src = "https://cubesniper24.github.io/bookmarklets/inspect/inspectScript.js";
+document.body.appendChild(scriptlink);

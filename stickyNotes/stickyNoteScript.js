aa4f0218dcadb5650944f813633a4f767331f350
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 11:09:26 -0400

    Update stickyNoteScript.js

--- stickyNotes/stickyNoteScript.js
+++ stickyNotes/stickyNoteScript.js
@@ -7,8 +7,8 @@ const dragHeader = document.createElement("DIV");
 //styling the main sticky
 
 note.style.backgroundColor = "yellow";
-note.style.width = "100%";
-note.style.height = "312%";
+note.style.width = "5%";
+note.style.height = "7.5%";
 note.style.border = "2px solid black";
 note.style.position = "absolute";
 dragHeader.appendChild(note);

6dd404c79ba624de34b3498558a22b4745910e8f
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 09:20:15 -0400

    Create bookmark.js

---
+++ stickyNotes/bookmark.js
@@ -0,0 +1,4 @@
+javascript:
+var scriptlink = document.createElement("script");
+scriptlink.src = "https://cubesniper24.github.io/bookmarklets/testing/inspectTesting/inspectScript.js";
+document.body.appendChild(scriptlink);

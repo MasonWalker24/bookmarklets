c1f4965eebb745ad4a6e37f9e820fc4c8da76cd2
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 09:57:46 -0400

    Update stickyNoteScript.js

--- stickyNotes/stickyNoteScript.js
+++ stickyNotes/stickyNoteScript.js
@@ -4,15 +4,19 @@ draggableObject.style.width = "15%";
 draggableObject.style.height = "30%";
 draggableObject.style.border = "2px solid black";
 draggableObject.style.position = "absolute";
+var addButton = document.createElement("BUTTON");
+addButton.style.width = "6%";
+addButton.style.height = "7.5%";
+addButton.style.position = "absolute";
+addButton.style.left = "94%";
+addButton.style.top = "92.5%";
+document.appendChild(addButton);
 
 
-document.addEventListener("keydown", addNote, false);
+
+addButton.addEventListener("click", addNote, false);
 
 function addNote(event)
 {
-  //if(event.keycode == 13)
-  //{
-    //document.appendChild(draggableObject);
-    alert("test");
-  //}
+    document.appendChild(draggableObject);
 }

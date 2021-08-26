d301f2afe9a99c59e473035fc859c51af328ddcb
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 10:20:20 -0400

    Update stickyNoteScript.js

--- stickyNotes/stickyNoteScript.js
+++ stickyNotes/stickyNoteScript.js
@@ -1,25 +1,30 @@
-const draggableObject = document.createElement("DIV");
-draggableObject.style.backgroundColor = "yellow";
-draggableObject.style.width = "15%";
-draggableObject.style.height = "30%";
-draggableObject.style.border = "2px solid black";
-draggableObject.style.position = "absolute";
+//adding the elements
+const note = document.createElement("DIV");
+const dragHeader = document.createElement("DIV");
 
-const addButton = document.createElement("BUTTON");
-addButton.style.width = "6%";
-addButton.style.height = "7.5%";
-addButton.style.position = "absolute";
-addButton.style.left = "94%";
-addButton.style.top = "92.5%";
-addButton.style.backgroundColor = "blue";
-document.body.appendChild(addButton);
+//styling the elements
 
+//styling the main sticky
+
+note.style.backgroundColor = "yellow";
+note.style.width = "100%";
+note.style.height = "312%";
+note.style.border = "2px solid black";
+note.style.position = "absolute";
+note.appendChild(note);
+
+//styling the header
+
+dragHeader.style.width = "5%";
+dragHeader.style.height = "2%";
+
+
+// adding the sticky note
 document.addEventListener("keydown", addNote, false);
-
 function addNote(event)
 {
     if(event.code == "ShiftRight")
     {
-        document.body.appendChild(draggableObject);
+        document.body.appendChild(dragHeader);
     }
 }

844f92912865db9801ae6f72e744b8b4b80e9d85
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 09:20:58 -0400

    Update bookmark.js

--- stickyNotes/bookmark.js
+++ stickyNotes/bookmark.js
@@ -1,4 +1,4 @@
 javascript:
 var scriptlink = document.createElement("script");
-scriptlink.src = "https://cubesniper24.github.io/bookmarklets/testing/inspectTesting/inspectScript.js";
+scriptlink.src = "https://cubesniper24.github.io/bookmarklets/stickyNotes/stickyNoteScript.js";
 document.body.appendChild(scriptlink);

5d495eaf6118f71760c99d4b6ab7fe7af2918b93
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-26 09:33:16 -0400

    Create stickyNoteScript.js

---
+++ stickyNotes/stickyNoteScript.js
@@ -0,0 +1,7 @@
+var draggableObject = document.createElement("DIV");
+draggableObject.style.backgroundColor = "yellow";
+draggableObject.style.width = "15%";
+draggableObject.style.height = "30%";
+draggableObject.style.border = "2px solid black";
+draggableObject.style.position = "absolute";
+document.body.appendChild(draggableObject);

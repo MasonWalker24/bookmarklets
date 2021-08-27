7d44062a9f4a8964d2a52c77e84fa4483e44f6fd
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-08-27 07:38:25 -0400

    Update stickyNoteScript.js

--- stickyNotes/stickyNoteScript.js
+++ stickyNotes/stickyNoteScript.js
@@ -1,6 +1,8 @@
 //adding the elements
 const note = document.createElement("DIV");
 const dragHeader = document.createElement("DIV");
+var att = document.createAttribute("contenteditable"); 
+att.value = "true";
 
 //styling the elements
 
@@ -10,14 +12,15 @@ note.style.backgroundColor = "yellow";
 note.style.width = "8%";
 note.style.height = "12%";
 note.style.border = "2px solid black";
-note.style.position = "absolute";
-dragHeader.appendChild(note);
+note.style.position = "fixed";
+note.setAttributeNode(att);
 
 //styling the header
 
 dragHeader.style.width = "8%";
 dragHeader.style.height = "3%";
 dragHeader.style.position = "absolute";
+dragHeader.appendChild(note);
 
 
 // adding the sticky note

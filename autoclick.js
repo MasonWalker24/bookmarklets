16373251540d47143072a51387ce9b0cfd736da8
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2022-01-19 08:15:20 -0500

    Update autoclick.js

--- autoclick.js
+++ autoclick.js
@@ -1,3 +1,14 @@
 javascript:
 var delay = prompt("delay(milliseconds)");
-alert(delay);
+var elementToClick;
+var elements = document.body.children;
+for(const element of elements)
+{
+  element.addEventListener("click", setclick, false);
+}
+
+function setclick(event)
+{
+  //elementToClick = event.target;
+  alert("yes");
+}
